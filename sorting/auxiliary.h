8fdcea0ab728ab8aa6dcb2ad3215c13c9975183d
Rayvenden <rayunderwater@gmail.com>
2015-11-12 22:34:10 +0100

    Additional module description

--- sorting/auxiliary.h
+++ sorting/auxiliary.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+/*
+ * It's auxiliary module with utilities. It's useful
+ * while checking correct of sort algorithms.
+ */
+
 void printIntArray(int *array, int len) {
     for (int i = 0; i < len; i++) {
         printf("%d ", array[i]);
